275756df91ba9d3261f800bc5d0c25f09108d392
John Brilla <jbrilla17@gmail.com>
2017-01-31 20:58:16 -0500

    starting anisble ffmpeg install

---
+++ VE-Setup/server-setup-ansible/ffmpeg/scripts/x265.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo apt-get install cmake mercurial
+cd ~/ffmpeg_sources
+hg clone https://bitbucket.org/multicoreware/x265
+cd ~/ffmpeg_sources/x265/build/linux
+PATH="$HOME/bin:$PATH" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
+make
+make install
+make distclean
---
+++ VE-Setup/server-setup-ansible/ffmpeg/scripts/yasm.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd ~/ffmpeg_sources
+wget http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
+tar xzvf yasm-1.3.0.tar.gz
+cd yasm-1.3.0
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/bin"
+make
+make install
+make distclean
